c8640d39e1f9ac1b2451aee55f1b7f270f4f4fce
macinjoke <shunji.makino@gmail.com>
2020-05-25 16:01:36 +0900

    update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,4 @@
 ln -s ~/.ghq/github.com/macinjoke/fish_config/config.fish ~/.config/fish/config.fish
 ln -s ~/.ghq/github.com/macinjoke/fish_config/functions ~/.config/fish/conf.d
+ln -s ~/.ghq/github.com/macinjoke/fish_config/fishfile ~/.config/fish/fishfile
+
